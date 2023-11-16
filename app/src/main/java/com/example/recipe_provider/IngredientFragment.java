74c65d1b3b4adf4d97380bf2cb140f3b57ef6345
kgj <kujin2028@gmail.com>
2023-11-16 23:42:55 +0900

    feat: fragment 기본 디자인 구성

--- app/src/main/java/com/example/recipe_provider/IngredientFragment.java
+++ app/src/main/java/com/example/recipe_provider/IngredientFragment.java
@@ -1,4 +1,4 @@
-package com.example.recipe_provider.;
+package com.example.recipe_provider;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -15,20 +15,10 @@ public class IngredientFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_view, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_ingredient, container, false);
 
-        ListView nameList = (ListView) rootView.findViewById(R.id.nameList);
-        ListView numList = (ListView) rootView.findViewById(R.id.numList);
-
-        //ListView와 MainActivity의 리스트를 연결하기 위한 어뎁터 정의
-        ArrayAdapter nameAdapter = new ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1,
-                ((MainActivity) requireActivity()).nameList);
-        ArrayAdapter numAdapter = new ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1,
-                ((MainActivity) requireActivity()).numList);
-
-        //각 리스트뷰를 어뎁터에 설정
-        nameList.setAdapter(nameAdapter);
-        numList.setAdapter(numAdapter);
+        ListView nameList = (ListView) rootView.findViewById(R.id.NameList);
+        ListView numList = (ListView) rootView.findViewById(R.id.AmountList);
 
         return rootView;
     }
