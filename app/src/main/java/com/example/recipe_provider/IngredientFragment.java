e48f16d1667f73ef8657b94a95e3db195e8451ba
kujin <kujin2028@gmail.com>
2023-11-16 15:40:58 +0900

    feat: 레시피, 재료 프래그먼트 추가

---
+++ app/src/main/java/com/example/recipe_provider/IngredientFragment.java
@@ -0,0 +1,36 @@
+package com.example.recipe_provider.;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class IngredientFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_view, container, false);
+
+        ListView nameList = (ListView) rootView.findViewById(R.id.nameList);
+        ListView numList = (ListView) rootView.findViewById(R.id.numList);
+
+        //ListView와 MainActivity의 리스트를 연결하기 위한 어뎁터 정의
+        ArrayAdapter nameAdapter = new ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1,
+                ((MainActivity) requireActivity()).nameList);
+        ArrayAdapter numAdapter = new ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1,
+                ((MainActivity) requireActivity()).numList);
+
+        //각 리스트뷰를 어뎁터에 설정
+        nameList.setAdapter(nameAdapter);
+        numList.setAdapter(numAdapter);
+
+        return rootView;
+    }
+
+}
