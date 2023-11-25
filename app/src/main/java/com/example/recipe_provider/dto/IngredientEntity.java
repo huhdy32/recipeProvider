9a3306f305ab5b5b1bd3e428fef6a61421a7324e
huhdy32 <mong3268@gmail.com>
2023-11-25 13:09:05 +0900

    feat: 재료 엔터티dto 추가

--- app/src/main/java/com/example/recipe_provider/dto/IngredientEntity.java
+++ app/src/main/java/com/example/recipe_provider/dto/IngredientEntity.java
@@ -1,2 +1,25 @@
-package com.example.recipe_provider.dto;public class IngredientEntity {
+package com.example.recipe_provider.dto;
+
+public class IngredientEntity {
+    private int id;
+    private String name;
+    private int remain;
+
+    public IngredientEntity(int id, String name, int remain) {
+        this.id = id;
+        this.name = name;
+        this.remain = remain;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRemain() {
+        return remain;
+    }
 }
