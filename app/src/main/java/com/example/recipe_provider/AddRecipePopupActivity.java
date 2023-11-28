f678c76f7fff2b841382d9dd8990e9e49fa31b5b
kujin <kujin2028@gmail.com>
2023-11-28 11:42:23 +0900

    kgj 브랜치 병합(파일 이름 통일)

---
+++ app/src/main/java/com/example/recipe_provider/AddRecipePopupActivity.java
@@ -0,0 +1,16 @@
+package com.example.recipe_provider;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class AddRecipePopupActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        // 타이틀 바 제거
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_add_reciepe);
+
+    }
+}
