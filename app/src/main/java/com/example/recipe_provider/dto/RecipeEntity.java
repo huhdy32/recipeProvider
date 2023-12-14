1fcdbbaca8759af23cf8c3ae6ce961ca041650fc
kujin <kujin2028@gmail.com>
2023-12-14 15:26:52 +0900

    제발요

--- app/src/main/java/com/example/recipe_provider/dto/RecipeEntity.java
+++ app/src/main/java/com/example/recipe_provider/dto/RecipeEntity.java
@@ -1,16 +1,17 @@
 package com.example.recipe_provider.dto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RecipeEntity {
     private final long id;
     private final String name;
-    private final int remain;
-    private final int require;
+    private final Map<Ingredient, Integer> ingredients;
 
-    public RecipeEntity(long id, String name, int remain, int require) {
+    public RecipeEntity(final long id, final String name, final HashMap<Ingredient, Integer> ingredients) {
         this.id = id;
         this.name = name;
-        this.remain = remain;
-        this.require = require;
+        this.ingredients = ingredients;
     }
 
     public long getId() {
@@ -20,10 +21,18 @@ public class RecipeEntity {
         return name;
     }
     public float getRate() {
-        float result = ((float) remain / (float) require) * 100;
-        if (result > 100) {
-            return 100;
+        int counts = ingredients.size();
+        float sum = 0;
+        for (Ingredient ingredient : ingredients.keySet()) {
+            if (ingredient.getRemain() == 0) {
+                continue;
+            }
+            float temp = (float) ingredient.getRemain() / (float) ingredients.get(ingredient) * 100;
+            if (temp > 100) {
+                temp = 100;
+            }
+            sum += temp / counts;
         }
-        return result;
+        return sum;
     }
 }
